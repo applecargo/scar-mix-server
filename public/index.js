30b042f1eee7cd8343f09a66e33e479034f47e8b
Dooho Yi <pilotedeguerre@gmail.com>
2020-11-19 18:04:55 +0900

    .

--- public/index.js
+++ public/index.js
@@ -141,6 +141,7 @@ $(document).ready(function() {
       }
       //set background colors
       $('body').css('background-color', screen_bgcolors[idx]);
+      console.log(screen_bgcolors[idx]);
     }
 
     function nextScreen() {
